d3e09208aded8fb20e9e160598cccbc5d81b2ea0
elgs <elgs1980@hotmail.com>
2016-03-05 05:15:20 -0800

    Update.

--- ip6.js
+++ ip6.js
@@ -5,13 +5,55 @@
     'use strict';
 
     var normalize = function (a) {
+        if (!_validate(a)) {
+            return false;
+        }
+        var nh = a.split(/\:\:/g);
+        if (nh.length > 2) {
+            return false;
+        }
+
+        var sections = [];
+        if (nh.length == 1) {
+            // full mode
+            sections = a.split(/\:/g);
+            if (sections.length !== 8) {
+                return false;
+            }
+        } else if (nh.length == 2) {
+            // compact mode
+            var n = nh[0];
+            var h = nh[1];
+            var ns = n.split(/\:/g);
+            var hs = h.split(/\:/g);
+            for (var i in ns) {
+                sections[i] = ns[i];
+            }
+            for (var i = hs.length; i > 0; --i) {
+                sections[7 - (hs.length - i)] = hs[i - 1];
+            }
+            for (var i = 0; i < 8; ++i) {
+                var section = sections[i];
+                if (section === undefined) {
+                    sections[i] = '0000';
+                }
+                if (sections[i].length < 4) {
+                    var pad = '0000';
+                    sections[i] = pad.substring(0, pad.length - section.length) + section;
+                }
+            }
+        }
+        return sections.join(':');
     };
     var compact = function (a) {
+        if (!_validate(a)) {
+            return false;
+        }
     };
 
     // Basic validation
     var _validate = function (a) {
-        return /^[a-f0-9\:]+$/ig.test(a);
+        return /^[a-f0-9\\:]+$/ig.test(a);
     };
 
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
--- ip6Spec.js
+++ ip6Spec.js
@@ -6,6 +6,9 @@
 
     var ip6 = require('./ip6.js');
 
+    ////////////////////////////////////////////////////////////////////////////
+    // Public tests
+
     describe('To normalize IPv6 addresses', function () {
         beforeEach(function () {
         });
@@ -13,7 +16,10 @@
         });
 
         it('should normalize IPv6 addresses', function () {
-            expect(1).toBe(1);
+            expect(ip6.normalize('2404:6800:4003:808::200e')).toBe('2404:6800:4003:0808:0000:0000:0000:200e');
+            expect(ip6.normalize('2404:6800:4003:0808:0000:0000:0000:200e')).toBe('2404:6800:4003:0808:0000:0000:0000:200e');
+            expect(ip6.normalize('2404:6800:4003:808::')).toBe('2404:6800:4003:0808:0000:0000:0000:0000');
+            expect(ip6.normalize('2404:68::')).toBe('2404:0068:0000:0000:0000:0000:0000:0000');
         });
     });
 
@@ -28,7 +34,10 @@
         });
     });
 
-    describe('To compact IPv6 addresses.', function () {
+    ////////////////////////////////////////////////////////////////////////////
+    // Private test
+
+    describe('To validate IPv6 addresses.', function () {
         beforeEach(function () {
         });
         afterEach(function () {
